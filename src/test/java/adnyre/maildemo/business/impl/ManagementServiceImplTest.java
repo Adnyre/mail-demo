28da2c53a02140f0d3bc46a954e3cb5735e260eb
adnyre <adnyre@gmail.com>
2018-05-24 23:12:55 +0300

    added more unit tests + pom cleanup

--- src/test/java/adnyre/maildemo/business/impl/ManagementServiceImplTest.java
+++ src/test/java/adnyre/maildemo/business/impl/ManagementServiceImplTest.java
@@ -13,6 +13,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static java.util.Arrays.asList;
@@ -56,18 +58,21 @@ public class ManagementServiceImplTest {
         user.getCampaigns().add(campaign);
         addressee1 = new Addressee();
         addressee1.setId(1);
+        addressee1.setEmail("email1");
         addressee2 = new Addressee();
         addressee2.setId(2);
+        addressee2.setEmail("email2");
         addressee3 = new Addressee();
         addressee3.setId(3);
+        addressee3.setEmail("email3");
         addressee4 = new Addressee();
         addressee4.setId(4);
+        addressee4.setEmail("email4");
+        campaign.getAddressees().addAll(asList(addressee1, addressee2));
     }
 
     @Test
     public void sendToNewAddresseesTest() {
-        campaign.getAddressees().addAll(asList(addressee1, addressee2));
-
         when(campaignService.get(1)).thenReturn(campaign);
         when(addresseeService.selectNewAddresseesForCampaign(1)).thenReturn(asList(addressee3, addressee4));
         when(mailService.sendSingleMessage(user, addressee3, template)).thenReturn(true);
@@ -86,16 +91,53 @@ public class ManagementServiceImplTest {
 
     @Test
     public void sendToAllTest() {
-        // TODO implement
+        when(campaignService.get(1)).thenReturn(campaign);
+        when(addresseeService.selectAllAddresseesForCampaign(1))
+                .thenReturn(asList(addressee1, addressee2, addressee3, addressee4));
+        when(mailService.sendSingleMessage(user, addressee1, template)).thenReturn(true);
+        when(mailService.sendSingleMessage(user, addressee2, template)).thenReturn(false);
+        when(mailService.sendSingleMessage(user, addressee3, template)).thenReturn(true);
+        when(mailService.sendSingleMessage(user, addressee4, template)).thenReturn(false);
+
+        managementService.sendToAll(1);
+
+        verify(campaignService).get(1);
+        verify(addresseeService).selectAllAddresseesForCampaign(1);
+        verify(mailService).sendSingleMessage(user, addressee1, template);
+        verify(mailService).sendSingleMessage(user, addressee2, template);
+        verify(mailService).sendSingleMessage(user, addressee3, template);
+        verify(mailService).sendSingleMessage(user, addressee4, template);
+        Set<Addressee> sentAddressees = campaign.getAddressees();
+        assertEquals(3, sentAddressees.size());
+        assertTrue(sentAddressees.containsAll(asList(addressee1, addressee2, addressee3)));
     }
 
     @Test
     public void resendTest() {
-        // TODO implement
+        when(campaignService.get(1)).thenReturn(campaign);
+
+        managementService.resend(1);
+
+        verify(campaignService).get(1);
+        verify(mailService).sendSingleMessage(user, addressee1, template);
+        verify(mailService).sendSingleMessage(user, addressee2, template);
+        Set<Addressee> sentAddressees = campaign.getAddressees();
+        assertEquals(2, sentAddressees.size());
+        assertTrue(sentAddressees.containsAll(asList(addressee1, addressee2)));
     }
 
     @Test
     public void checkEmailTest() {
-        // TODO implement
+        List<String> emails = asList("email1", "email4", "email5", "email8");
+        when(userService.get(1)).thenReturn(user);
+        when(mailService.checkEmail(user)).thenReturn(emails);
+        when(addresseeService.getByEmails(new HashSet<>(emails))).thenReturn(asList(addressee1, addressee4));
+
+        managementService.checkEmail(1);
+
+        verify(userService).get(1);
+        verify(mailService).checkEmail(user);
+        verify(addresseeService).getByEmails(new HashSet<>(emails));
+        assertTrue(user.getAddressees().containsAll(asList(addressee1, addressee4)));
     }
 }
